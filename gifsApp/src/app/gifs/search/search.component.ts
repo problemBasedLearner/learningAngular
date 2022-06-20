808172322ca6cbccb40f6f29f386e7ac9bba23c5
Pablo Murga <pmurga@biolanmb.com>
2022-06-20 13:12:16 +0200

    WIP Gifs

--- gifsApp/src/app/gifs/search/search.component.ts
+++ gifsApp/src/app/gifs/search/search.component.ts
@@ -1,7 +1,5 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
-
-
-  
+import { GifsService } from '../services/gifs.service';
 
 
 @Component({
@@ -9,16 +7,19 @@ import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
- 
 export class SearchComponent {
 
-  constructor() { }
+  searchTopic: string = ''
 
-  searchTopic:string = ''
-
+  // By using constructor we create gifsService obj by instanciating GifsService class
+  // GifsService is defined in ../services/gifs.service.ts
+  constructor( private gifsService: GifsService ) {}
 
   searchGif() {
-    console.log(this.searchTopic);
-    this.searchTopic = ''  
+    if (this.searchTopic.trim() != '') {
+      this.gifsService.buscarGifs( this.searchTopic )
+      // console.log(this.searchTopic);
+      this.searchTopic = ''
+    }
   }
 }
--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -3,8 +3,25 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ServicesService {
 
-  constructor() { }
+export class GifsService {
+
+  private apiKey : string = 'Tb51KUh1cZC4Rh1qTbWpd8itCMjYBWTI'
+
+  private _historialArr : string[] = []
+
+  // Adding get method to the class we can access the content of the function by calling it as any other class key
+  // https://www.youtube.com/watch?v=bl98dm7vJt0
+  // In this case the function creates a new array with the existing historial array spreaded
+  get historial() {
+    return [...this._historialArr]
+  }
+
+  buscarGifs( searchTopic : string ) {
+    if (!this._historialArr.map(a => a.toLowerCase()).includes(searchTopic.toLowerCase())) {
+      this._historialArr.unshift( searchTopic )
+      console.log(this._historialArr)
+      this._historialArr = this._historialArr.splice(0,10)  
+    }
+  }
 }
-asdfaafsd
